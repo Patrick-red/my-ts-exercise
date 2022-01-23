10181a6d310d888eae7ad24e45d85fe420c8246f
Patrick-red <1492634056@qq.com>
2022-01-23 22:52:30 +0800

    add infer to 119

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -1,7 +1,7 @@
 type ReplaceAll<S extends string, From extends string, To extends string> = 
   From extends ''
   ? S
-  : S extends `${infer H}${ From }${ T }`
+  : S extends `${infer H}${ From }${infer T }`
     ? `${H}${To}${ReplaceAll<T, From, To>}`
     : S
 
